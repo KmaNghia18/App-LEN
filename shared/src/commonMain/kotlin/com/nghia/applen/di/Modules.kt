dbd8adbb114e8027c9cca27cae2bc8b506f4a06e
KmaNghia18 <nghia@example.com>
2026-01-30 17:57:14 +0700

    feat: add Koin dependency injection modules

---
+++ shared/src/commonMain/kotlin/com/nghia/applen/di/Modules.kt
@@ -0,0 +1,20 @@
+package com.nghia.applen.di
+
+import com.nghia.applen.data.local.DatabaseDriverFactory
+import com.nghia.applen.data.repository.VocabularyRepository
+import com.nghia.applen.db.AppDatabase
+import com.nghia.applen.domain.SpacedRepetitionEngine
+import org.koin.dsl.module
+
+val databaseModule = module {
+    single { DatabaseDriverFactory(get()).createDriver() }
+    single { AppDatabase(get()) }
+}
+
+val domainModule = module {
+    single { SpacedRepetitionEngine() }
+}
+
+val dataModule = module {
+    single { VocabularyRepository(get(), get()) }
+}
