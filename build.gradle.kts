b6f2e5435329addedfb98d74820fdec45a4f646a
KmaNghia18 <nghia@example.com>
2026-01-30 14:41:39 +0700

    chore: setup root build.gradle.kts with plugin versions

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    // Kotlin Multiplatform
+    kotlin("multiplatform") version "1.9.22" apply false
+    kotlin("plugin.serialization") version "1.9.22" apply false
+    
+    // Android
+    id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
+    
+    // SQLDelight
+    id("app.cash.sqldelight") version "2.0.1" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
