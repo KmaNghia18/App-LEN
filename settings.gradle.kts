0ebfd4730f7fe74ad65d127452a30c473d4b193e
KmaNghia18 <nghia@example.com>
2026-01-30 14:41:36 +0700

    chore: configure project modules in settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+rootProject.name = "App-LEN"
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":androidApp")
+include(":shared")
